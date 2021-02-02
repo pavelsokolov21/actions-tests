1a75bb390ff94abc16340ab0689bdf74450fb9e0
Pavel Sokolov <49731235+SMPlay@users.noreply.github.com>
2021-02-02 20:07:24 +0300

    testing

---
+++ src/utils/sum.spec.js
@@ -0,0 +1,11 @@
+import { sum } from "./sum";
+
+describe("sum", () => {
+  it("should return 4", () => {
+    expect(sum(2, 2)).toBe(4);
+  });
+
+  it("should return 5", () => {
+    expect(sum(2, 2)).toBe(5);
+  });
+});
