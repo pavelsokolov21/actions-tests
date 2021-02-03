d012f31be2ecb88a439bead66b85798c9f9183fd
Pavel Sokolov <49731235+SMPlay@users.noreply.github.com>
2021-02-03 12:34:12 +0300

    Add check lint actions

--- src/utils/sum.spec.js
+++ src/utils/sum.spec.js
@@ -1,11 +1,11 @@
-import { sum } from "./sum";
+import { sum } from './sum';
 
-describe("sum", () => {
-  it("should return 4", () => {
+describe('sum', () => {
+  it('should return 4', () => {
     expect(sum(2, 2)).toBe(4);
   });
 
-  it("should return 5", () => {
+  it('should return 5', () => {
     expect(sum(2, 3)).toBe(5);
   });
 });
